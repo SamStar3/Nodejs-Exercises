37b703be5c909635c19da81d35ace1d86dd8d685
SamStar3 <samstar1044948@gmail.com>
2024-08-28 21:23:15 +0530

    file added

--- index.js
+++ index.js
@@ -9,35 +9,37 @@
 //     logEvents(msg);
 // });
 // myEmitter.emit('log', 'log event emitted');
-const express = require("express");
-const app = express();
-const jobs = {}
+// const express = require("express");
+// const app = express();
+// const jobs = {}
     
-app.post("/submit", (req, res) => {
-    const jobId = `job:${Date.now()}`
-    jobs[jobId] = 0;
-    updateJob(jobId, 0); 
-    res.end("\n\n" + jobId + "\n\n");
-});
+// app.post("/submit", (req, res) => {
+//     const jobId = `job:${Date.now()}`
+//     jobs[jobId] = 0;
+//     updateJob(jobId, 0); 
+//     res.end("\n\n" + jobId + "\n\n");
+// });
 
-app.get("/checkstatus", (req, res) => {
-    console.log("Received Job ID:", req.query.jobId); // Debugging line
-    console.log(jobs[req.query.jobId]);
-    res.end("\n\nJobStatus: " + jobs[req.query.jobId] + "%\n\n");
-});
+// app.get("/checkstatus", (req, res) => {
+//     console.log("Received Job ID:", req.query.jobId); // Debugging line
+//     console.log(jobs[req.query.jobId]);
+//     res.end("\n\nJobStatus: " + jobs[req.query.jobId] + "%\n\n");
+// });
 
-// Root URL route
-app.get("/", (req, res) => {
-    res.send("Welcome to the Job Status API! Use /submit to create a job and /checkstatus to check the job status.");
-});
+// // Root URL route
+// app.get("/", (req, res) => {
+//     res.send("Welcome to the Job Status API! Use /submit to create a job and /checkstatus to check the job status.");
+// });
 
-app.listen(3000, () => console.log("listening on 3000"));
+// app.listen(3000, () => console.log("listening on 3000"));
 
-function updateJob(jobId, prg) {
-    jobs[jobId] = prg;
-    console.log(`updated ${jobId} to ${prg}`);
-    if (prg == 100) return;
-    setTimeout(() => updateJob(jobId, prg + 10), 3000);
-}
+// function updateJob(jobId, prg) {
+//     jobs[jobId] = prg;
+//     console.log(`updated ${jobId} to ${prg}`);
+//     if (prg == 100) return;
+//     setTimeout(() => updateJob(jobId, prg + 10), 3000);
+// }
 
 // http://localhost:3000/checkstatus?
+
+
