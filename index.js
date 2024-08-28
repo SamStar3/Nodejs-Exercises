3807230bc529e8b0c77888e76041703adb45cf93
SamStar3 <samstar1044948@gmail.com>
2024-08-28 21:29:01 +0530

    file added

--- index.js
+++ index.js
@@ -43,3 +43,26 @@
 // http://localhost:3000/checkstatus?
 
 
+const http = require("http");
+const WebSocketServer = require("websocket").server
+let connections = [];
+
+const httpserver = http.createServer()
+
+const websocket = new WebSocketServer({"httpServer": httpserver })
+
+httpserver.listen(8080, () => console.log("My server is listening on port 8080"))
+
+websocket.on("request", request=> {
+
+    const connection = request.accept(null, request.origin)
+    connection.on("message", message => {
+        //someone just sent a message tell everybody
+        connections.forEach (c=> c.send(`User${connection.socket.remotePort} says: ${message.utf8Data}`))
+    }) 
+    
+    connections.push(connection)
+    //someone just connected, tell everybody
+    connections.forEach (c=> c.send(`User${connection.socket.remotePort} just connected.`))
+  
+})
