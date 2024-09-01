2f75c6ed41acf9ffc1e781f4ac8aa9c658201300
SamStar3 <samstar1044948@gmail.com>
2024-09-01 18:50:03 +0530

    file added

--- index.js
+++ index.js
@@ -42,39 +42,39 @@
 
 // http://localhost:3000/checkstatus?
 
-const http = require("http");
-const WebSocketServer = require("websocket").server;
+// const http = require("http");
+// const WebSocketServer = require("websocket").server;
 
-let connections = [];
+// let connections = [];
 
-const httpserver = http.createServer();
+// const httpserver = http.createServer();
 
-const websocket = new WebSocketServer({ "httpServer": httpserver });
+// const websocket = new WebSocketServer({ "httpServer": httpserver });
 
-httpserver.listen(8080, () => console.log("My server is listening on port 8080"));
+// httpserver.listen(8080, () => console.log("My server is listening on port 8080"));
 
-websocket.on("request", (request) => {
-    const connection = request.accept(null, request.origin);
+// websocket.on("request", (request) => {
+//     const connection = request.accept(null, request.origin);
 
-    connections.push(connection);
-    connections.forEach(c => c.send(`User${connection.socket.remotePort} just connected.`));
+//     connections.push(connection);
+//     connections.forEach(c => c.send(`User${connection.socket.remotePort} just connected.`));
 
-    connection.on("message", (message) => {
-        connections.forEach(c => c.send(`User${connection.socket.remotePort} says: ${message.utf8Data}`));
-    });
+//     connection.on("message", (message) => {
+//         connections.forEach(c => c.send(`User${connection.socket.remotePort} says: ${message.utf8Data}`));
+//     });
 
-    connection.on("close", () => {
-        // Remove the connection from the list
-        connections = connections.filter(c => c !== connection);
+//     connection.on("close", () => {
+//         // Remove the connection from the list
+//         connections = connections.filter(c => c !== connection);
 
-        // Announce disconnection to all users
-        connections.forEach(c => c.send(`User${connection.socket.remotePort} has disconnected.`));
-    });
+//         // Announce disconnection to all users
+//         connections.forEach(c => c.send(`User${connection.socket.remotePort} has disconnected.`));
+//     });
 
-    connection.on("error", (error) => {
-        console.error(`Error occurred: ${error.message}`);
-    });
-});
+//     connection.on("error", (error) => {
+//         console.error(`Error occurred: ${error.message}`);
+//     });
+// });
 
 
 
@@ -82,3 +82,69 @@ websocket.on("request", (request) => {
 //let ws = new WebSocket("ws://localhost:8080");
 //ws.onmessage = message => console.log(`Received: ${message.data}`);
 //ws.send("Hello! I'm client")
+
+
+// const app = require("express")();
+// const jobs = {}
+
+// app.post("/submit", (req, res) =>  {
+//     const jobId = `job:${Date.now()}`
+//     jobs[jobId] = 0;
+//     updateJob(jobId,0);
+//     res.end("\n\n" + jobId + "\n\n");
+// })
+
+// app.get("/checkstatus", async (req, res) => {
+//     console.log(jobs[req.query.jobId])
+//     //long polling, don't respond until done
+//     while(await checkJobComplete(req.query.jobId) == false);
+//     res.end("\n\nJobStatus: Complete " + jobs[req.query.jobId] + "%\n\n")
+
+// } )
+
+// app.listen(8080, () => console.log("listening on 8080"));
+
+// async function checkJobComplete(jobId) {
+//     return new Promise( (resolve, reject) => {
+//         if (jobs[jobId] < 100)
+//             this.setTimeout(()=> resolve(false),  1000);
+//         else
+//             resolve(true);
+//     })
+   
+// }
+
+// function updateJob(jobId, prg) {
+//     jobs[jobId] = prg;
+//     console.log(`updated ${jobId} to ${prg}`)
+//     if (prg == 100) return;
+//     this.setTimeout(()=> updateJob(jobId, prg + 10), 10000)
+// }
+
+// http://localhost:8080/checkstatus?jobId=
+
+
+
+const app = require("express")();
+
+app.get("/", (req, res) => res.send("hello!"));
+
+app.get("/stream", (req,res) => {
+
+    res.setHeader("Content-Type", "text/event-stream");
+    send(res);
+
+})
+const port = process.env.PORT || 8888;
+
+let i = 0;
+function send (res) {
+    
+    res.write("data: " + `hello from server ---- [${i++}]\n\n`);
+
+
+    setTimeout(() => send(res), 1000);
+}
+
+app.listen(port)
+console.log(`Listening on ${port}`)
